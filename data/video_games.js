fd46def48b6d08f7d367d1bc61a90262bae36032
GhostRiley <corentin.maris@gmail.com>
2024-10-15 16:55:10 +0200

    ajout d'images + modifs js

--- data/video_games.js
+++ data/video_games.js
@@ -1,21 +1,45 @@
 const videoGameDatas = 
 [
     {
-        "name" : "R-Type Effect",
-        "image1": "../../img/video-games/r_type_effect.jpg",
-        "link": "video-games/r_type_effect.html",
-        "filter-type": "school"
+        "name" : "QATP",
+        "image1": "../../img/video-games/qatp.jpg",
+        "link": "video-games/qatp.html",
+        "filter-type": "jam"
     },  
     {
-        "name" : "Walking Simulator",
-        "image1": "../../img/video-games/walking_simulator.jpg",
-        "link": "video-games/walking_simulator.html",
-        "filter-type": "school"
+        "name" : "SeedRails",
+        "image1": "../../img/video-games/seedRails.jpg",
+        "link": "video-games/SeedRails.html",
+        "filter-type": "jam"
     },   
     {
         "name" : "Immun Escape",
         "image1": "../../img/video-games/immun_escape.jpg",
         "link": "video-games/immun_escape.html",
         "filter-type": "school"
-    }
+    },
+    {
+        "name" : "R-Type Effect",
+        "image1": "../../img/video-games/r_type_effect.jpg",
+        "link": "video-games/r_type_effect.html",
+        "filter-type": "school"
+    },  
+    {
+        "name" : "Misdirection : Nature's Last Hope",
+        "image1": "../../img/video-games/misdirection.jpg",
+        "link": "video-games/misdirection.html",
+        "filter-type": "school"
+    },   
+    {
+        "name" : "House of Desolation : Lotus Society",
+        "image1": "../../img/video-games/house_of_desolation.jpg",
+        "link": "video-games/house_of_desolation.html",
+        "filter-type": "school"
+    },
+    {
+        "name" : "Hebald Forest : The Last Journey",
+        "image1": "../../img/video-games/hebald_forest.jpg",
+        "link": "video-games/hebald_forest.html",
+        "filter-type": "school"
+    },
 ]
--- js/video_game_system.js
+++ js/video_game_system.js
@@ -1,22 +1,94 @@
 let videoGameElements = [];
+let videoGameLinks = [];
 let videoGameContainer = document.getElementById("video-game-container");
 
 for (let i = 0; i < videoGameDatas.length; i++) {
-    var _videoGameProject = document.createElement("div");
-    _videoGameProject.setAttribute('class', 'video-game-card object-to-filter ' + videoGameDatas[i]["filter-type"]);
+  var _videoGameCardContainer = document.createElement("div");
+  _videoGameCardContainer.setAttribute(
+    "class",
+    "video-game-card-container object-to-filter " +
+      videoGameDatas[i]["filter-type"]
+  );
+  var _videoGameProject = _videoGameCardContainer.appendChild(
+    document.createElement("div")
+  );
+  _videoGameProject.setAttribute("class", "video-game-card");
 
-    _videoGameProject.appendChild(Object.assign(document.createElement('div'), {classList: 'video-game-card-arrow'}));
+  _videoGameProject.appendChild(
+    Object.assign(document.createElement("img"), {
+      classList: "video-game-card-img text-zone",
+      src: videoGameDatas[i].image1,
+      id: i,
+    })
+  );
+  // var scrollDiv = _videoGameProject.appendChild(Object.assign(document.createElement('div'), {classList: 'video-game-scroll-div'}));
+  // var scrollContainer = scrollDiv.appendChild(Object.assign(document.createElement('div'), {classList: 'video-game-scroll-container'}));
+  // scrollContainer.appendChild(Object.assign(document.createElement('h4'), {classList: 'video-game-scroll-text1'}, {innerHTML: videoGameDatas[i].name + ' • '}));
+  // scrollContainer.appendChild(Object.assign(document.createElement('h4'), {classList: 'video-game-scroll-text2'}, {innerHTML: videoGameDatas[i].name + ' • '}));
+  var _videoGameCardDiv = _videoGameCardContainer.appendChild(
+    Object.assign(document.createElement("div"), {
+      classList: "video-game-card-bottom",
+    })
+  );
+  _videoGameCardDiv.appendChild(
+    Object.assign(document.createElement("b"), {
+      innerHTML: videoGameDatas[i].name,
+    })
+  );
+  if (videoGameDatas[i]["tags"] != "") {
+    var _tagDiv = _videoGameProject.appendChild(
+      Object.assign(
+        document.createElement("div"),
+        { classList: "video-game-tag-div" }
+      )
+    );
+    var _scrollDiv = _tagDiv.appendChild(
+      Object.assign(
+        document.createElement("div"),
+        { classList: "scroll-text-new-div" }
+      )
+    );
+    var _sizeDiv =_scrollDiv.appendChild(
+      Object.assign(
+        document.createElement("div"),
+        { style: "width: 1000px;" }
+      )
+    );
+    var _textDivA =_sizeDiv.appendChild(
+      Object.assign(
+        document.createElement("div"),
+        { classList: "scroll-text-new" }
+      )
+    );
+    _textDivA.appendChild(
+      Object.assign(
+        document.createElement("h5"),
+        { innerHTML: videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " +  videoGameDatas[i]["tags"] + " • " +  videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • "}
+      )
+    );
+    var _textDivB =_sizeDiv.appendChild(
+      Object.assign(
+        document.createElement("div"),
+        { classList: "scroll-text-new" }
+      )
+    );
+    _textDivB.appendChild(
+      Object.assign(
+        document.createElement("h5"),
+        { innerHTML: videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " +  videoGameDatas[i]["tags"] + " • " +  videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • " + videoGameDatas[i]["tags"] + " • "}
+      )
+    );
+  }
 
-    _videoGameProject.appendChild(Object.assign(document.createElement('img'), {classList: 'video-game-card-img', src: videoGameDatas[i].image1}));
-    var scrollDiv = _videoGameProject.appendChild(Object.assign(document.createElement('div'), {classList: 'video-game-scroll-div'}));
-    var scrollContainer = scrollDiv.appendChild(Object.assign(document.createElement('div'), {classList: 'video-game-scroll-container'}));
-    scrollContainer.appendChild(Object.assign(document.createElement('h4'), {classList: 'video-game-scroll-text1'}, {innerHTML: videoGameDatas[i].name + ' • '}));
-    scrollContainer.appendChild(Object.assign(document.createElement('h4'), {classList: 'video-game-scroll-text2'}, {innerHTML: videoGameDatas[i].name + ' • '}));
-    _videoGameProject.appendChild(Object.assign(document.createElement('a'), {href: videoGameDatas[i].link, innerHTML: '►'}));
+  var videoGameLink = _videoGameProject.appendChild(
+    Object.assign(document.createElement("a"), {
+      href: videoGameDatas[i].link,
+      style: "display:none",
+    })
+  );
 
-    videoGameContainer.appendChild(_videoGameProject);
+  videoGameContainer.appendChild(_videoGameCardContainer);
 
-
-    videoGameElements.push(_videoGameProject);
-    
-} 
+  videoGameElements.push(_videoGameProject);
+  videoGameLinks.push(videoGameLink);
+}
