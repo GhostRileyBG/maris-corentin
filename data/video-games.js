100e3e598a39c46b35b6ec0e2b2ac02c694e3481
GhostRiley <corentin.maris@gmail.com>
2024-10-27 00:54:50 +0200

    Update video-games.js

--- data/video-games.js
+++ data/video-games.js
@@ -2,44 +2,44 @@ const videoGameDatas =
 [
     {
         "name" : "QATP",
-        "image1": "../../img/video-games/qatp.jpg",
+        "image1": "../img/video-games/qatp.jpg",
         "link": "video-games/qatp.html",
         "filter-type": "jam",
         // "tags": ""
     },  
     {
         "name" : "SeedRails",
-        "image1": "../../img/video-games/seed-rails1.png",
+        "image1": "../img/video-games/seed-rails1.png",
         "link": "video-games/seed-rails.html",
         "filter-type": "jam"
     },   
     {
         "name" : "Immun Escape",
-        "image1": "../../img/video-games/immun-escape-logo.png",
+        "image1": "../img/video-games/immun-escape-logo.png",
         "link": "video-games/immun-escape.html",
         "filter-type": "school"
     },
     {
         "name" : "R-Type Effect",
-        "image1": "../../img/video-games/r-type-effect-logo.png",
+        "image1": "../img/video-games/r-type-effect-logo.png",
         "link": "video-games/r-type-effect.html",
         "filter-type": "school"
     },  
     {
         "name" : "Misdirection : Nature's Last Hope",
-        "image1": "../../img/video-games/misdirection.png",
+        "image1": "../img/video-games/misdirection.png",
         "link": "video-games/misdirection.html",
         "filter-type": "school"
     },   
     {
         "name" : "House of Desolation : Lotus Society",
-        "image1": "../../img/video-games/house-of-desolation-poster.png",
+        "image1": "../img/video-games/house-of-desolation-poster.png",
         "link": "video-games/house-of-desolation.html",
         "filter-type": "school"
     },
     {
         "name" : "Hebald Forest : The Last Journey",
-        "image1": "../../img/video-games/hebald-forest-poster.webp",
+        "image1": "../img/video-games/hebald-forest-poster.webp",
         "link": "video-games/hebald-forest.html",
         "filter-type": "school"
     },
